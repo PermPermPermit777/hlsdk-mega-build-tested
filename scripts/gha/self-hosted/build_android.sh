842241bf9117655823ac9c5ff99d49647ac76400
PermPermPermit777 <bangconaaquenia0@gmail.com>
2026-05-10 12:42:01 +0800

    Point to r29 path android ndk

--- scripts/gha/self-hosted/build_android.sh
+++ scripts/gha/self-hosted/build_android.sh
@@ -8,7 +8,7 @@ WAF_CONFIGURE_PLATFORM_OPTS[amd64]=--android=x86_64,clang,21
 WAF_CONFIGURE_PLATFORM_OPTS[arm64]=--android=arm64-v8a,clang,21
 WAF_CONFIGURE_PLATFORM_OPTS[armel]=--android=armeabi-v7a,clang,21
 
-export ANDROID_NDK_HOME=/opt/android/ndk/28.2.13676358/
+export ANDROID_NDK_HOME=/opt/android/ndk/29.0.14206865/
 
 WAF_CONFIGURE_OPTS=${WAF_CONFIGURE_PLATFORM_OPTS[$GH_CPU_ARCH]}
 
